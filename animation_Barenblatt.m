4108066a61d6a64c760b0b19359ca924d8062860
trygvba <trygve.baerland@gmail.com>
2014-09-18 16:50:21 +0200

    Changed animation script to generate a single figure.

--- animation_Barenblatt.m
+++ animation_Barenblatt.m
@@ -19,13 +19,15 @@ dt = T/nt;
 
 %Spatial domain:
 x = linspace(-2,2,100);
-
+t  = linspace(0, 0.5, 3)
 figure
-for i=1:nt
-    t = dt*i;
-    U = t^(-alpha) * max(0,C-k*abs(x).^2*t^(-2*beta)).^(1/(m-1));
-    pause(0.03)
+axis([-2 2 0 3])
+xlabel('x-axis')
+ylabel('u')
+title('Example of solution for different times')
+hold on
+for dt=linspace(0.01, 0.5, 3)
+    U = dt^(-alpha) * max(0,C-k*abs(x).^2*dt^(-2*beta)).^(1/(m-1));
     plot(x,U)
-    axis([-2 2 0 10])
-    drawnow
 end
+hold off
